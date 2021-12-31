852689f3564d60cb98a12d2dca54c838b0459ee2
Reaperking <72340160+What1sLov3RK@users.noreply.github.com>
2021-12-31 10:18:15 +0200

    Delete pz1_2.py

--- pz1_2.py
+++
@@ -1,7 +0,0 @@
-import operator
-import sys
-ds={'add': operator.add, 'sub': operator.sub, 'div': operator.truediv, "mul": operator.mul}
-first_num = int(sys.argv[2])
-operation = str(sys.argv[1])
-second_num = int(sys.argv[3])
-print(ds[operation](first_num, second_num))
